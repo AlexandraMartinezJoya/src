b63dedfde9ce058d05de5f9ffc997aeb3c4e06a3
Alexa <za0ne78@hotmail.com>
2021-07-04 17:22:17 +0300

    Update Pyramid.java

--- main/java/Chapter4/Pyramid.java
+++ main/java/Chapter4/Pyramid.java
@@ -1,5 +1,13 @@
 package Chapter4;
 
+/** A comment 
+Had to leave this commment here. Apparently I was complaining that a whole lot of programmers I've met write the crappiest code ever immagined and then they go into
+meetings explaining how they build the Pyramids, the Hanging Gardens, or any other world wonder. It's become one of my main jokes, since I've began testing.
+Then here comes this exercise, and I had to build, a Pyramid. I couldn't do it. :D I have used the code of some stranger online, which really has solved the Pyramid puzzle.
+So, I would give him credit, and I would, had I know where to refer him. for right now, hes just a stealth nickname leaving a comment on some forum. Anyhow, thank you stranger. 
+Apparently you and only you have indeed build a Pyramid :) 
+*/
+
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
