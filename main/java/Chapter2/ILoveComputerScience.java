f5fa23a34aa63e726c177e4880558a27955ddbaf
Alexa <za0ne78@hotmail.com>
2021-08-20 21:35:16 +0300

    :)

---
+++ main/java/Chapter2/ILoveComputerScience.java
@@ -0,0 +1,29 @@
+package Chapter2;
+import acm.graphics.GLabel;
+
+class ILoveComputerScience {
+
+
+
+    public void run(){
+        GLabel myLabel = new GLabel("I love myself and my son.");
+        GLabel myOtherLabel = new GLabel("I also love programming and helping other people grow and succeed at what they do");
+        GLabel myThirdLabel = new GLabel("I also love my friends.");
+        GLabel myFouthLabel = new GLabel("I also love the family on my side.");
+        GLabel myFifthLabel = new GLabel("I loved many guys in my past and slept with them all and do not regret it one bit. I love making love! Am very good at it too.");
+        GLabel mySixthLabel = new GLabel("I do not want to wear a chastity belt for enjoying having sex or love-making, be tortured, punished, almost killed because I own my own body");
+        GLabel mySeventhLabel = new GLabel("And any religion that does that to human beings should not exist.");
+
+        add(myLabel);
+        add(myOtherLabel);
+        add(myThirdLabel);
+        add(myFouthLabel);
+        add(myFifthLabel);
+        add(mySixthLabel);
+        add(mySeventhLabel);
+    }
+
+    private void add(GLabel label){
+
+    }
+}
