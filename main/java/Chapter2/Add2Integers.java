4c0b00581c76c9e614ad25155fd7da8b7ea36abe
Alexa <za0ne78@hotmail.com>
2021-09-24 01:12:24 +0300

    Update Add2Integers.java

--- main/java/Chapter2/Add2Integers.java
+++ main/java/Chapter2/Add2Integers.java
@@ -2,13 +2,19 @@ package Chapter2;
 
 import acm.program.*;
 
+/**
+ * This program is one of the exercises found in the Java book.
+ * It uses the java reserved keyword for integers.
+ *
+ * Solved by @AlexandraMartinezJoya
+ */
 public class Add2Integers extends ConsoleProgram{
 
     public void run(){
         println("This program adds two numbers. It can only read 10 digits");
-        int fistNumber = readInt("Please type in the first number and press enter key : ");
-        int secondNumber = readInt("Please type in the second number and press enter key:  ");
-        int addedNumbers = fistNumber + secondNumber;
-        println("The total is " + addedNumbers + ".");
+        int clinker = readInt("Please type the amount of clinked used for 20kg of cement: ");
+        int water = readInt("Please type in the amount of water needed for 20kgs of cement:  ");
+        int cement = clinker + water;
+        println("The total mixing quantities of cement is " + cement + ".");
     }
 }
