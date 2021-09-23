f9328fa96a61db1f6761be66f48e1c42fbe5829d
Alexa <za0ne78@hotmail.com>
2021-09-24 01:22:44 +0300

    Update OlympicRings.java

--- main/java/Chapter2/OlympicRings.java
+++ main/java/Chapter2/OlympicRings.java
@@ -5,12 +5,18 @@ import acm.program.GraphicsProgram;
 
 import java.awt.*;
 
+/***
+ * This program is displaying Olympic Rings
+ *
+ * Solved by @AlexandraMartinezJoya
+ */
+
 public class OlympicRings extends GraphicsProgram {
 
-    private Color olympicsBlue = new Color(14, 119, 177);
-    private Color olympicsYellow = new Color(255, 178, 60);
-    private Color olympicsGreen = new Color(8, 173, 81);
-    private Color olympicsRed = new Color(237, 54, 74);
+    private final Color olympicsBlue = new Color(14, 119, 177);
+    private final Color olympicsYellow = new Color(255, 178, 60);
+    private final Color olympicsGreen = new Color(8, 173, 81);
+    private final Color olympicsRed = new Color(237, 54, 74);
 
     public void run(){
         GOval blueRing = new GOval(50, 50, 150, 150);
