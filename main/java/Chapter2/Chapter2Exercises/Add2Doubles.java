2067719ea78519489be5cabea5f9a72d40205540
Alexa <za0ne78@hotmail.com>
2021-09-24 01:11:26 +0300

    Create Add2Doubles.java

---
+++ main/java/Chapter2/Chapter2Exercises/Add2Doubles.java
@@ -0,0 +1,14 @@
+package Chapter2.Chapter2Exercises;
+
+import acm.program.ConsoleProgram;
+
+/***
+ * This program adds two by two doubles. It uses the java reserved keyword for doubles
+ *
+ */
+public class Add2Doubles extends ConsoleProgram {
+
+    public void run(){
+        println("This program should add two doubles");
+    }
+}
