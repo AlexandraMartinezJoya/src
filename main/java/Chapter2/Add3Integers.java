a99345270b7ba261f7ae007897890846b44dce38
Alexa <za0ne78@hotmail.com>
2021-09-24 01:12:51 +0300

    Update Add3Integers.java

--- main/java/Chapter2/Add3Integers.java
+++ main/java/Chapter2/Add3Integers.java
@@ -2,14 +2,20 @@ package Chapter2;
 
 import acm.program.ConsoleProgram;
 
+/***
+ * This program adds three integers. It uses the java reserved keyword for integers
+ *
+ * Solved by @AlexandraMartinezJoya
+ */
+
 public class Add3Integers extends ConsoleProgram {
 
     public void run(){
         println("This program adds two numbers. It can only read 10 digits");
-        int fistNumber = readInt("Please type in the first number and press enter key : ");
-        int secondNumber = readInt("Please type in the second number and press enter key:  ");
-        int thirdNumber = readInt("Please type in the third number and press enter key:  ");
-        int addedNumbers = fistNumber + secondNumber + thirdNumber;
-        println("The total is " + addedNumbers + ".");
+        int fives = readInt("Please type in the first number and press enter key : ");
+        int tens = readInt("Please type in the second number and press enter key:  ");
+        int twenties = readInt("Please type in the third number and press enter key:  ");
+        int addedInts = fives + tens + twenties;
+        println("The total is " + addedInts + ".");
     }
 }
