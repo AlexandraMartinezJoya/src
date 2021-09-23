9452c3965bcf4441d777597b40b44070ebc564e2
Alexa <za0ne78@hotmail.com>
2021-09-24 01:11:49 +0300

    Update Add2Doubles.java

--- main/java/Chapter2/Add2Doubles.java
+++ main/java/Chapter2/Add2Doubles.java
@@ -2,13 +2,24 @@ package Chapter2;
 
 import acm.program.*;
 
+/***
+ * This program adds two by two doubles. It uses the java reserved keyword for doubles
+ *
+ * Solved by @AlexandraMartinezJoya
+ */
 public class Add2Doubles extends ConsoleProgram {
 
     public void run(){
         println("this program adds two doubles");
-        double number1 = readDouble("Enter first number: ");
-        double number2 = readDouble("Enter second number: ");
-        double total = number1 + number2;
-        println("The total is " + total + ".");
+        double flour = readDouble("Enter first number: ");
+        double eggs = readDouble("Enter second number: ");
+        double butter = readDouble("Enter second number: ");
+        double raisins = readDouble("Enter second number: ");
+
+        double flourRaisins = flour + raisins;
+        double butteredEggs = eggs + butter;
+
+        double newCakeDough = flourRaisins + butteredEggs;
+        println("The new dough recipie weights: " + newCakeDough + ".");
     }
 }
